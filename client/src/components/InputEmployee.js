4aaf045540fd54a9a7515afaf293f1577bf4cda7
Christos Rentzos <rentzosc@gmail.com>
2023-10-17 20:00:33 +0300

    input

--- client/src/components/InputEmployee.js
+++ client/src/components/InputEmployee.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, {Fragment, useState} from "react";
 
 
 const InputEmployee = () => {
+    
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [employeeID, setEmployeeID] = useState("");
+    
     return (
-        <h1> Input Employeev</h1>
+        <Fragment>
+            <h1 className="text-center my-5"> Input Employee</h1>
+            <form>
+                <input 
+                    type="text" 
+                    placeholder="First name" 
+                    className="form-control" 
+                    value={firstName}
+                />
+                
+                <input 
+                    type="text" 
+                    placeholder="Last name" 
+                    className="form-control" 
+                    value={lastName}
+                />
+                
+                <input 
+                    type="text" 
+                    placeholder="Employee ID" 
+                    className="form-control" 
+                    value={employeeID} 
+                />
+                
+                <button className="btn btn-success">Submit</button>
+            </form>
+        </Fragment>
     )
 }
 
