c079399f5cf408d569a5b7b7edf96d78477c0971
Christos Rentzos <rentzosc@gmail.com>
2023-10-17 14:26:41 +0300

    starting react app

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, {Fragment} from 'react';
+import './App.css';
+import InputEmployee from './components/InputEmployee';
+
+
+function App() {
+  return (
+    <Fragment>
+      <div className='container'>
+        <InputEmployee/>
+      </div>
+    </Fragment>
+  )
+}
+
+
+export default App;
---
+++ client/src/components/InputEmployee.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const InputEmployee = () => {
+    return (
+        <h1>Input Employee</h1>
+    )
+}
+
+export default  InputEmployee;
