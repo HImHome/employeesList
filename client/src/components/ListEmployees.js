6a2959979eb7fa593df45ae720a0eab777567604
Christos Rentzos <rentzosc@gmail.com>
2023-10-19 20:50:40 +0300

    delete button

--- client/src/components/ListEmployees.js
+++ client/src/components/ListEmployees.js
@@ -1,9 +1,23 @@
 import React, {Fragment, useState, useEffect} from "react";
 
 const ListEmployees = () =>{
-    
     const [employees, setEmployees] = useState([]);
 
+    //delete employee function
+    async function deleteEmployee(id) {
+        try {
+           const res = await fetch(`http://localhost:5000/employees/${id}`,{
+                method: "DELETE"
+            });
+            
+            setEmployees(employees.filter(employee => employee.employee_id !== id));
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+    
+    
+    
     async function getEmployees () {
         const res = await fetch("http://localhost:5000/employees");
 
@@ -36,7 +50,7 @@ const ListEmployees = () =>{
                 {
                     employees.map((employee) => (
 
-                        <tr>
+                        <tr key = {employee.employee_id}>
                             <th>{ employee.employee_id }</th>
                             <td>{ employee.first_name}</td>
                             <td>{ employee.last_name }</td>
@@ -46,8 +60,7 @@ const ListEmployees = () =>{
                                 </button>
                             </td>
                             <td>
-                                <button className="btn btn-danger">Delete
-                                </button>
+                                <button className="btn btn-danger" onClick={() => deleteEmployee(employee.employee_id)}>Delete</button>
                             </td>
                         </tr>
                         
